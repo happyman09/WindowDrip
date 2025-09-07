9932794f9610cde91426c90962f54d2a762fa34f
happyman09 <125260988+happyman09@users.noreply.github.com>
2025-09-07 19:48:12 +0530

    Updated to match new main.py

--- utils/colors.py
+++ utils/colors.py
@@ -1,7 +1,10 @@
 from colorthief import ColorThief
 
-def get_dominant_color(image_path: str) -> str:
-    """Extract dominant color from wallpaper as HEX string."""
+def get_dominant_color(image_path):
     color_thief = ColorThief(image_path)
-    dominant = color_thief.get_color(quality=1)
-    return "#{:02x}{:02x}{:02x}".format(*dominant)
+    dominant_color = color_thief.get_color(quality=1)
+    return dominant_color  # (R, G, B)
+
+def rgb_to_hex(rgb):
+    """Convert (R, G, B) → #RRGGBB"""
+    return "#{:02x}{:02x}{:02x}".format(rgb[0], rgb[1], rgb[2])
